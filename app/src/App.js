2686a9fe09890dc4b7f8877577bbd1e4d3ddd447
Eduardo Sorókin <eduardosorokin@gmail.com>
2019-03-28 02:08:14 +0000

    Sending the challenges

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from "react";
+import ImgBotcamp from "./components/ImgBotcamp";
 import BtnBotcamp from "./components/BtnBotcamp";
+import "./App.css"
 
 class App extends Component {
   render() {
-    return <BtnBotcamp/>
+    return [
+      <ImgBotcamp/>,
+      <BtnBotcamp/>
+    ];
   }
 }
 
---
+++ app/src/components/ImgBotcamp/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./style.css";
+
+const ImgBotcamp = () => (
+  <img src="/img/botcamp.svg" className="img-botcamp" alt="BotCamp"/>
+);
+
+export default ImgBotcamp;
