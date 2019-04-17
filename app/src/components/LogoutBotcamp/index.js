5bec26d6ec46370d0f63a6d6233a643c48490e1d
Eduardo Sorókin <eduardosorokin@gmail.com>
2019-04-17 01:51:00 +0000

    Sending the challenges

---
+++ app/src/components/LogoutBotcamp/index.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+import logout from "./../../img/logout.svg";
+
+const LogoutBotcamp = styled.img.attrs({
+  src: logout,
+  alt: "Logout"
+})``;
+
+export default LogoutBotcamp;
